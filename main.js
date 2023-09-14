fe019298176207020feda90fe6f36f579cb7d7e7
David Pron <92371541+DavidPron@users.noreply.github.com>
2023-09-14 23:02:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// var data = confirm("Ты сейчас дома?")
+// if(data) {
+//     confirm("Cool")
+// } else {
+//     confirm("Bad")
+// }
+
+// var person = null
+// if (confirm("Вы уверены?")) {
+//     person = prompt("Укажите ваше Имя");
+//     alert("Привет " + person);
+// } else {
+//     alert("Вы нажали <отмена>")
+// }
+
+// function info() {
+//     console.log(3 + 3 + (3 * 3) * 14 / 2)
+// }
+
+// function info(word) {
+//     console.log("#" + word)
+// }
+
+// function summa(a, b) {
+//     var res = a * b;
+//     info(res);
+// }
+
+// summa(13, 45)
+
+
+for (i = 100; i >= -10; i--) {
+    console.log(i)
+}
